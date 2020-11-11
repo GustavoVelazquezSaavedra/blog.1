f40fef1988b5a9472eefd3a65a8127c7226d5ae5
Gustavo Velazquez <gabrivelaz@hotmail.com>
2020-11-11 20:04:12 -0300

    Agregando annotate bundle, y dejando anotaciones en el proyecto posts

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :integer          not null
+#
+# Indexes
+#
+#  index_comments_on_post_id  (post_id)
+#
+# Foreign Keys
+#
+#  post_id  (post_id => posts.id)
+#
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
